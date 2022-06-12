38b46d602d2df7de429e02e52aeb9a4b1112e4a7
Shakhawat Hossain <shakhawathossain.se@gmail.com>
2022-06-12 12:35:10 +0600

    added a new version of cleanup

---
+++ batch files/Cleanup v.5.1.bat
@@ -0,0 +1,27 @@
+*********START CODE************
+
+@ECHO OFF
+
+del C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Recent Items*.* /Q pause
+
+del /s /f /q C:\Windows\Prefetch\*.*
+
+del /s /f /q C:\Windows\Temp\*.*
+
+del /s /f /q %USERPROFILE%\appdata\local\temp\*.*
+
+rd /s /q %systemdrive%\$Recycle.bin
+
+
+/Below command to Show the folder after deleted files
+
+Explorer %userprofile%\Recent
+
+Explorer C:\Windows\Prefetch
+
+Explorer C:\Windows\Temp
+
+Explorer %USERPROFILE%\appdata\local\temp
+
+
+*********END CODE************
